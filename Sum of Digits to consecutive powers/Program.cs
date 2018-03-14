efafddda99089ad17fc5c70477087eaf3b903b1e
dowenb00 <34041313+dowenb00@users.noreply.github.com>
2018-03-14 16:21:59 -0500

    Add project files.

---
+++ Sum of Digits to consecutive powers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Find the sum of a string's digits to consecutive powers. e.g., 735 => 7^1 + 3^2 + 5^3.
+namespace Sum_of_Digits_to_consecutive_powers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long a = 1;
+            long b = 135;
+            int result = 0;
+            List<long> nums = new List<long> { };
+
+            
+
+            for (long i = a; i <= b; i++)
+            {
+                char[] digits = (i).ToString().ToCharArray();
+                
+                for (int x = 0; x < digits.Length; x++)
+                {
+                    result += (int)Math.Pow(Char.GetNumericValue(digits[x]), x + 1);
+                }
+
+                if (result == i)
+                {
+                    nums.Add(i);
+                }
+                
+
+                result = 0;
+            }
+
+            foreach (long n in nums)
+            {
+                Console.WriteLine(n);
+            }
+            long[] final = nums.ToArray();
+        }
+    }
+}
